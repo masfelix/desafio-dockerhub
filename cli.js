175e0037577aa65bf6fffe6c5124925fc9e765d1
Marcos Felix <masfelix@gmail.com>
2021-12-13 18:34:55 -0300

    Initial commit

---
+++ cli.js
@@ -0,0 +1,3 @@
+"use strict";delete process.env.VSCODE_CWD;const bootstrap=require("./bootstrap"),bootstrapNode=require("./bootstrap-node"),product=require("../product.json");bootstrap.avoidMonkeyPatchFromAppInsights(),bootstrapNode.configurePortable(product),bootstrap.enableASARSupport(),process.env.VSCODE_CLI="1",require("./bootstrap-amd").load("vs/code/node/cli");
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/core/cli.js.map
